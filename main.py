8b66d151877c3e20e5edf875f2e82e4e158be44b
timzi <timzik06@gmail.com>
2022-04-10 21:54:14 +0300

    форма регистрации 2

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from loginform import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
