ea7824b93171b18a38e3ac6666e23fa0d37648c1
timzi <timzik06@gmail.com>
2022-04-10 15:50:06 +0300

    форма регистрации

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'placeholder'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
